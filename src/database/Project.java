29e7e968f124df9dac54f0d84a4fdcb0d5d4463f
JesperAnnefors <jesper_annefors@hotmail.com>
2020-03-06 21:38:47 +0100

    Add model for Project.

---
+++ src/database/Project.java
@@ -0,0 +1,18 @@
+package database;
+
+public class Project {
+	private int projectId;
+	private String name;
+	
+	public int getProjectId() {
+		return this.projectId;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
