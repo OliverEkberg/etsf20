06db36bf5a498362edf60d546a0c63dc5bf8f40b
OliverEkberg <oliver.ekberg1889@gmail.com>
2020-03-06 21:36:06 +0100

    Finish roles model

--- src/database/Roles.java
+++ src/database/Roles.java
@@ -1,5 +1,14 @@
 package database;
 
 public class Roles {
-
+	private int roleId;
+	private String role;
+	
+	public int getRoleId() {
+		return roleId;
+	}
+	
+	public String getRole() {
+		return role;
+	}
 }
