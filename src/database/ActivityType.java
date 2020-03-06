f545a5f4179b17ceca4ce11c01a8230eb15f2b3a
OliverEkberg <oliver.ekberg1889@gmail.com>
2020-03-06 21:38:02 +0100

    Add model for ActivityType

---
+++ src/database/ActivityType.java
@@ -0,0 +1,14 @@
+package database;
+
+public class ActivityType {
+	private int activityTypeId;
+	private String type;
+	
+	public int getActivityTypeId() {
+		return activityTypeId;
+	}
+	
+	public String getType() {
+		return type;
+	}
+}
