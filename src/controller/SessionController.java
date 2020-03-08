a388988af6f88e5124e0f24d63d7039c8dce1efb
feritbolezek <developer@feritbolezek.com>
2020-03-09 00:09:55 +0100

    Added simple styling and setup for login check.

--- src/controller/SessionController.java
+++ src/controller/SessionController.java
@@ -36,17 +36,45 @@ public class SessionController extends servletBase {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
 		out.println(getPageIntro());
-		out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"StyleSheets/SessionController.css\">");
 		
-		out.println("    <div class=\"wrapper\">\r\n" + 
-				"        <div class=\"credentials_form\">\r\n" + 
-				"            Hello World\r\n" + 
-				"        </div>\r\n" + 
-				"    </div>"
-				);
 
-		out.println("</body></html>");
+		String name = req.getParameter("username"); // get the string that the user entered in the form
+		String password = req.getParameter("password"); // get the entered password
+		
+		System.out.println(name);
+		System.out.println(password);
+        
+        
+		if (name != null && password != null) {
+			
+			// TODO: Login check...
+			
+			out.println(loginRequestForm()); // TODO: Remove this later.
+			
+		} else { 
+			out.println(loginRequestForm());
+		}	
+		
     }
+		
+	private String loginRequestForm() {
+			return 	"<link rel=\"stylesheet\" type=\"text/css\" href=\"StyleSheets/SessionController.css\">" +
+			
+			    "<div class=\"wrapper\">\r\n" + 
+					"        <div class=\"title\">TimeKeep</div>\r\n" + 
+					"        <div class=\"subTitle\">Keep track of time and stuff.</div>\r\n" + 
+					"        <div class=\"center credentials_form\">\r\n" + 
+					"            <form>\r\n" + 
+					"                <input class=\"credentials_rect\" type=\"text\" name=\"username\" placeholder=\"Username\" required><br>\r\n" + 
+					"                <input class=\"credentials_rect\" type=\"text\" name=\"password\" placeholder=\"Password\" required><br><br>\r\n" + 
+					"                <input class=\"submitBtn\" type=\"submit\" value=\"Submit\">\r\n" + 
+					"              </form> \r\n" + 
+					"        </div>\r\n" + 
+					"        <div class=\"footerText\">Developed by some dudes at LTH.</div>\r\n" + 
+					"    </div>"
+
+			+"</body></html>";
+	}
     
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
