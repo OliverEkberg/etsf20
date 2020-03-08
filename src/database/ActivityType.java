af6ffa1e6eed0158b3399f01463236d9365586ae
JesperAnnefors <jesper_annefors@hotmail.com>
2020-03-08 15:41:32 +0100

    add method for getActivityTypes

--- src/database/ActivityType.java
+++ src/database/ActivityType.java
@@ -4,6 +4,11 @@ public class ActivityType {
 	private int activityTypeId;
 	private String type;
 	
+	public ActivityType(int activityTypeId, String type) {
+		this.activityTypeId = activityTypeId;
+		this.type = type;
+	}
+
 	public int getActivityTypeId() {
 		return activityTypeId;
 	}
