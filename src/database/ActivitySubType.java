14dfd4f7a538ad959d71c6c061eee2e03d7b76cd
JesperAnnefors <jesper_annefors@hotmail.com>
2020-03-08 14:06:29 +0100

    Add model for ActivitySubType

---
+++ src/database/ActivitySubType.java
@@ -0,0 +1,22 @@
+package database;
+
+public class ActivitySubType {
+	private int activitySubTypeId;
+	private int activityTypeId;
+	private String subType;
+	
+	public int getActivitySubTypeId() {
+		return activitySubTypeId;
+	}
+	
+	public int getActivityTypeId() {
+		return activityTypeId;
+	}
+	
+	public String getSubType() {
+		return subType;
+	}
+	
+	
+	
+}
