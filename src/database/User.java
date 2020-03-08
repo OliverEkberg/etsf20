31e4ae91e5b77ca7dc09ec45d94d55a5c7758f84
JesperAnnefors <jesper_annefors@hotmail.com>
2020-03-08 14:16:45 +0100

    add model for User

---
+++ src/database/User.java
@@ -0,0 +1,36 @@
+package database;
+
+public class User {
+	private int userId;
+	private String username;
+	private String password;
+	private boolean isAdmin;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	
+	public void setIsAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+}
