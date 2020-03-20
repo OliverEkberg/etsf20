5624819d4304cf8f744c544a750b8c299f570316
Oliver Ekberg <oliver.ekberg1889@gmail.com>
2020-03-20 14:13:26 +0100

    sortUserList skapad som en statisk metod (#32)
    Co-authored-by: André <André@10.10.96.219>

--- src/controller/Helpers.java
+++ src/controller/Helpers.java
@@ -3,7 +3,11 @@ package controller;
 import java.time.LocalDate;
 import java.time.temporal.TemporalField;
 import java.time.temporal.WeekFields;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
+
+import database.User;
 
 /**
  * Provides a set of helper methods that are to be used throughout the whole system.
@@ -37,5 +41,19 @@ public class Helpers {
 	public static int getWeekNbr(LocalDate dateInWeek) {
 		TemporalField woy = WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear(); 
 		return dateInWeek.get(woy);
+	}
+	
+	/**
+	 * Sorts a list of Users alphabeticaly.
+	 *  
+	 * @param userList - The list to be sorted
+	 * @return a sorted list of users.
+	 */
+	public static List<User> sortUserList(List<User> userList) { 
+
+		Comparator<User> comparator = (u1, u2) -> u2.getUsername().compareTo(u1.getUsername());
+		userList.sort(comparator);
+
+		return userList;
 	}
 }
